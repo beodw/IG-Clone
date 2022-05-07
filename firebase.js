24b4ecceb43cf21a23e0b98eb8dc2f30a06fc535
beodwilson <beodwilson@gmail.com>
2022-05-07 11:33:38 +0000

    add auth secret

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApp, getApps } from "firebase/app";
-import { getFirestore } from 'firebase/firestore';
-import { getStorage } from "firebase/storage";
+import { initializeApp, getApp, getApps } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -9,18 +9,17 @@ import { getStorage } from "firebase/storage";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
-  authDomain: "igclone-2de98.firebaseapp.com",
-  projectId: "igclone-2de98",
-  storageBucket: "igclone-2de98.appspot.com",
-  messagingSenderId: "331497761810",
+  authDomain: 'igclone-2de98.firebaseapp.com',
+  projectId: 'igclone-2de98',
+  storageBucket: 'igclone-2de98.appspot.com',
+  messagingSenderId: '331497761810',
   appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID
-};
+  measurementId: process.env.MEASUREMENT_ID,
+}
 
 // Initialize Firebase
-const app = !getApps().length ?  initializeApp(firebaseConfig) : getApp();
-const  fireStoreDB = getFirestore();
-const firebaseStorage = getStorage();
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
+const fireStoreDB = getFirestore()
+const firebaseStorage = getStorage()
 
-
-export { app, fireStoreDB, firebaseStorage}; 
+export { app, fireStoreDB, firebaseStorage }
